c39513d750a1322076b9df6a8a6286914046c998
navid hashemi <navid.hashemi.t@gmail.com>
2015-02-12 19:34:56 -0500

    Final Project1

--- FTPClient/src/FTPClient.java
+++ FTPClient/src/FTPClient.java
@@ -2,26 +2,26 @@ import java.io.*;
 import java.net.*;
 
 public class FTPClient {
-       public static void main(String[] args){
-       	      if (args.length !=2){
-       	    	  System.err.println("Usage: java EchoClient cf2.cs.uga.edu 8080");
-       	    	  System.exit(1);
-	      }
-	      
-	      String hostName = args[0];
-	      int portNumber = Integer.parseInt(args[1]);
-	      
-	      try(	    		  
-	    		  Socket mySocket = new Socket (hostName, portNumber);	    		  
-	    	      DataInputStream inCon = new DataInputStream(mySocket.getInputStream());
-	    	      BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
-	    	      DataOutputStream outCon = new DataOutputStream(mySocket.getOutputStream());	    		 
-			){
-	    	  
+	public static void main(String[] args){
+		if (args.length !=2){
+			System.err.println("Usage: java EchoClient cf2.cs.uga.edu 8080");
+			System.exit(1);
+		}
+
+		String hostName = args[0];
+		int portNumber = Integer.parseInt(args[1]);
+
+		try(	    		  
+				Socket mySocket = new Socket (hostName, portNumber);	    		  
+				DataInputStream inCon = new DataInputStream(mySocket.getInputStream());
+				BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
+				DataOutputStream outCon = new DataOutputStream(mySocket.getOutputStream());	    		 
+				){
+
 			String userInput;				
 			System.out.print("ftp:/> ");			
 			while ((userInput = console.readLine()) != null ){
-				
+
 				String cmdName="";
 				String paraName ="";				
 
@@ -35,10 +35,10 @@ public class FTPClient {
 				else{
 					cmdName = userInput;
 				}
-				
+
 				switch (cmdName){
 				case "put":
-					
+
 					putData(paraName,inCon, outCon);
 					//System.out.println ( inCon.readUTF());
 					System.out.print("ftp:/> ");
@@ -56,53 +56,67 @@ public class FTPClient {
 					mySocket.close();
 					System.out.println("Goodbye!");
 					System.exit(1);
+					break;					
 				default:
 					outCon.writeUTF(userInput);
-					System.out.println ( inCon.readUTF());
+					//					inCon.readUTF()? "dd":"ff";
+
+					String result="";
+					String serverResult;
+					serverResult = inCon.readUTF();
+					if (serverResult.equals("true"))
+						result = cmdName + " Done!";
+					else
+						result = serverResult;
+					System.out.println ( result);
 					System.out.print("ftp:/> ");
 				}										
-				
-				
+
+
 			} 
 		}
 		catch (UnknownHostException e){		      
-		      System.err.println("Don't know about host " + hostName);
-		      System.exit(1);
+			System.err.println("Don't know about host " + hostName);
+			System.exit(1);
 		}		catch (IOException e){
-		      System.err.println("Couldn't get I/O for the connection to " + hostName );
-		      System.exit (1);
+			System.err.println("Couldn't get I/O for the connection to " + hostName );
+			System.exit (1);
 		}
 
-       }
-       
-       public static void putData(String fileName,DataInputStream in ,DataOutputStream out ) throws IOException{
-    	   File file = new File(fileName);
-    	   if (!file.exists()){
-    		   System.out.println("No file exists!");
-    		   return;
-    	   }
-    	   out.writeUTF("put " + fileName);
-    	   String response = in.readUTF();
+	}
 
-    	   if (!response.equals("ready")){
-    		   System.out.println("File already exists in Server!");
-    		   return;
-    	   }
-    	   
-    	   
-    	   try {
-			BufferedReader br = new BufferedReader(new FileReader(fileName));
-			int line;
-			do{
-				line = br.read();
-				out.writeUTF(String.valueOf(line));
+	public static void putData(String fileName,DataInputStream in ,DataOutputStream out ) throws IOException{
+		File file = new File(fileName);
+		if (!file.exists()){
+			System.out.println("No file exists!");
+			return;
+		}
+		out.writeUTF("put " + fileName);
+		String response = in.readUTF();
+
+		if (!response.equals("ready")){
+			System.out.println("File already exists in Server!");
+			return;
+		}
+
+
+		try {
+			int count;
+			byte[] buffer = new byte[1024];
+			BufferedInputStream br = new BufferedInputStream(new FileInputStream(fileName));
+			while ((count = br.read(buffer)) > 0) {
+				out.write(buffer, 0, count);
+				if (in.readUTF().equals("next")){
+				}
 				
-			}while (line != -1);
+			}
+			
+
 			br.close();
 			response = in.readUTF();
-			
+
 			System.out.println("Transfer successful!");
-			
+
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -110,73 +124,67 @@ public class FTPClient {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-    	   
-    	   
-       }
-       
-       public static String readFile(String fileName) throws IOException {
-    	    BufferedReader br = new BufferedReader(new FileReader(fileName));
-    	    try {
-    	        StringBuilder sb = new StringBuilder();
-    	        String line = br.readLine();
-
-    	        while (line != null) {
-    	            sb.append(line);
-    	            sb.append("\n");
-    	            line = br.readLine();
-    	        }
-    	        return sb.toString();
-    	    } finally {
-    	        br.close();
-    	    }
-    	}
-       
-       
-       public static void getData(String fileName, DataInputStream in, DataOutputStream out){
-
-   		File file = new File(fileName);
-   		
-   		if (file.exists()){
-   			
-   			try {
-   				out.writeUTF("file exists!");
-   			} catch (IOException e) {
-   				
-   				// TODO Auto-generated catch block
-   				e.printStackTrace();
-   			}
-
-   		}
-   		else{
-   			try {
-   				out.writeUTF("get " + fileName);
-   				String temp = in.readUTF();
-   				if (temp.equals("okey")){
-   					
-   					FileOutputStream fileStream= new FileOutputStream(file);
-   					int intInput ;
-   					String input;
-   					out.writeUTF("ready");
-   					do{
-   						input = in.readUTF();
-   						intInput = Integer.parseInt(input);
-   						if (intInput!=-1) fileStream.write(intInput);
-   					}while (intInput != -1);
-
-   					fileStream.close();
-
-   				}
-   				else out.writeUTF("error");
-   				
-   				System.out.println(in.readUTF());
-   					
-   			} catch (IOException e) {
-   				// TODO Auto-generated catch block
-   				e.printStackTrace();
-   			}
-   			
-   		}
 
 
-   	}
+	}
+
+	public static String readFile(String fileName) throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader(fileName));
+		try {
+			StringBuilder sb = new StringBuilder();
+			String line = br.readLine();
+
+			while (line != null) {
+				sb.append(line);
+				sb.append("\n");
+				line = br.readLine();
+			}
+			return sb.toString();
+		} finally {
+			br.close();
+		}
+	}
+
+
+	public static void getData(String fileName, DataInputStream in, DataOutputStream out){
+
+		File file = new File(fileName);
+		if (file.exists()){   			
+			System.out.println("file exists on client!");
+		}
+		else{
+			try {
+				out.writeUTF("get " + fileName);
+				String temp = in.readUTF();
+				if (temp.equals("okey")){
+					out.writeUTF("ready");
+					FileOutputStream fileStream= new FileOutputStream(file);
+					byte[] buffer = new byte[1024];
+					int count;
+					long j=1;
+					do{
+						count=in.read(buffer);
+						fileStream.write(buffer, 0, count);
+						out.writeUTF("next");
+						//System.out.print(j);
+						//j++;
+					}while (count/1024>0);
+					//String Result = in.readUTF();
+					//System.out.println(Result);
+					fileStream.close();
+					System.out.println("Transfer successful!");
+				}
+				else System.out.println("Error occured on receiving!");
+
+				//				System.out.println("Transfer successful!");
+
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+		}
+
+
+	}
 }
--- FTPServer/src/FTPServer.java
+++ FTPServer/src/FTPServer.java
@@ -12,7 +12,7 @@ public class FTPServer {
 		int portNumber = Integer.parseInt(args[0]);
 
 		try (
-				ServerSocket serverSocket = new ServerSocket(Integer.parseInt(args[0]));
+				ServerSocket serverSocket = new ServerSocket(portNumber);
 				Socket clientSocket = serverSocket.accept(); 
 
 				DataInputStream in = new DataInputStream(clientSocket.getInputStream());
@@ -20,63 +20,70 @@ public class FTPServer {
 
 				) {
 			String inputLine;
-			while ((inputLine = in.readUTF()) != null) {
-				String cmdName="";
-				String paraName ="";
-				boolean result=false;
 
-				inputLine = inputLine.trim();
-				int i = inputLine.indexOf(" ");
+			while (true) {
+				if (!clientSocket.isClosed()){
+					inputLine = in.readUTF();
+					//System.out.println("<" + inputLine + ">" );
+					String cmdName="";
+					String paraName ="";
+					boolean result=false;
 
-				if (i != -1){
-					cmdName = inputLine.substring(0, i);
-					paraName = inputLine.substring(i+1 , inputLine.length());
+					inputLine = inputLine.trim();
+					int i = inputLine.indexOf(" ");
+
+					if (i != -1){
+						cmdName = inputLine.substring(0, i);
+						paraName = inputLine.substring(i+1 , inputLine.length());
+					}
+					else{
+						cmdName = inputLine;
+					}
+
+					switch (cmdName){
+					case "mkdir":					
+						result = CreateDirectory(paraName);
+						out.writeUTF( String.valueOf(result));
+						break;
+					case "delete":
+
+						out.writeUTF( DeleteDirectory(paraName));
+						break;
+					case "ls":
+						out.writeUTF( ListDirectory());
+						break;
+					case "pwd":
+						out.writeUTF( CurrentDirectory());
+						break;
+					case "cd":
+						out.writeUTF(ChangeDirectory (paraName));
+						break;
+					case "put":
+						receiveFile(paraName, in, out);
+						break;	
+					case "get":
+						SendFile(paraName, in, out);
+						break;
+					case "quit":
+						//					out.close();
+						//					in.close();
+						//					serverSocket.close();
+						//					System.exit(1);
+						clientSocket.close();
+						break;
+					default:
+						out.writeUTF(cmdName + ": command not found");
+						break;
+					}
+
+
 				}
-				else{
-					cmdName = inputLine;
-				}
-
-				switch (cmdName){
-				case "mkdir":					
-					result = CreateDirectory(paraName);
-					out.writeUTF( String.valueOf(result));
-					break;
-				case "delete":
-					result = DeleteDirectory(paraName);
-					out.writeUTF( String.valueOf(result));
-					break;
-				case "ls":
-					out.writeUTF( ListDirectory());
-					break;
-				case "pwd":
-					out.writeUTF( CurrentDirectory());
-					break;
-				case "cd":
-					out.writeUTF(ChangeDirectory (paraName));
-					break;
-				case "put":
-					receiveFile(paraName, in, out);
-					break;	
-				case "get":
-					SendFile(paraName, in, out);
-					break;
-				case "quit":
-					out.close();
-					in.close();
-					serverSocket.close();
-					System.exit(1);
-					
-				default:
-					out.writeUTF(cmdName + ": command not found");
-					break;
-				}
-
-
 			}
 		} catch (IOException e) {
-			System.out.println("Exception caught when trying to listen on port "
-					+ portNumber + " or listening for a connection");
+			//			System.out.println("Exception caught when trying to listen on port "
+			//					+ portNumber + " or listening for a connection");
 			System.out.println(e.getMessage());
+			//			System.out.println(e);
 		}
 
 
@@ -87,13 +94,16 @@ public class FTPServer {
 		return dir.mkdir();				
 	}
 
-	public static boolean DeleteDirectory(String directoryName){
+	public static String DeleteDirectory(String directoryName){
 		try{
 			File dir = new File(directoryName);
-			return dir.delete();
+			if (!dir.exists()){
+				return "No Such File exists!";
+			}
+			return String.valueOf(dir.delete());
 		}
 		catch (Exception e ){
-			return false;
+			return "No such file exists";
 		}
 
 	}
@@ -112,13 +122,13 @@ public class FTPServer {
 		String pwd = System.getProperty("user.dir");
 		return pwd;
 	}
-	
+
 	public static void receiveFile(String fileName, DataInputStream in, DataOutputStream out){
 
 		File file = new File(fileName);
-		
+
 		if (file.exists()){
-			System.out.println("in IF");
+		    //System.out.println("in IF");
 			try {
 				out.writeUTF("file exists!");
 			} catch (IOException e) {
@@ -131,99 +141,130 @@ public class FTPServer {
 			try {
 				out.writeUTF("ready");
 				FileOutputStream fileStream= new FileOutputStream(file);
-				int intInput ;
-				String input;
+
+				byte[] buffer = new byte[1024];
+				int count;
+				long j=1;
 				do{
-					input = in.readUTF();
-					intInput = Integer.parseInt(input);
-					if (intInput!=-1) fileStream.write(intInput);
-				}while (intInput != -1);
+					count=in.read(buffer);
+					fileStream.write(buffer, 0, count);
+					//out.writeUTF("ready");
+					out.writeUTF("next");
+					
+					//System.out.println(j + "-" + count);
+					//j++;
+				}while (count/1024>0);
+
 				
 				fileStream.close();
-				
+
 				out.writeUTF("Done!");
-				
+
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
-			
+
 		}
 
 
 	}
 
-	public static String ChangeDirectory(String directory){
-		try {
+	public static String ChangeDirectory(String path){
 
+		String[] directories = path.split("/");
+		//		String current
+		for (String directory: directories){
+			try {
+				if(!directory.isEmpty()){
+					File dir;
 
-			if(!directory.isEmpty()){
-				File dir;
+					if (directory.equals("..")){
+						dir = new File (System.getProperty("user.dir"));
 
-				if (directory.equals("..")){
-					dir = new File (System.getProperty("user.dir"));
+						String ParentName = dir.getParentFile().getAbsolutePath();
 
-					String ParentName = dir.getParentFile().getAbsolutePath();
+						System.setProperty("user.dir", ParentName);
+						//						String pwd = System.getProperty("user.dir");
+						//						return pwd;
 
-					System.setProperty("user.dir", ParentName);
-					String pwd = System.getProperty("user.dir");
-					return pwd;
-
-				}
-				else{
-					dir = new File(directory);
-					if(dir.isDirectory() == true){
-						System.setProperty("user.dir", dir.getAbsolutePath());
-						String pwd = System.getProperty("user.dir");
-						return pwd;
 					}
 					else{
-						return directory + " : No such a directory found!";
+						dir = new File(System.getProperty("user.dir"),directory );
+						//						System.out.println(dir.getAbsoluteFile().getPath());
+						if(dir.isDirectory()){
+							System.setProperty("user.dir", dir.getAbsoluteFile().getPath());
+							//							System.out.println(directory);
+							//							System.out.println(dir.getAbsoluteFile().getPath());
+							//System.out.println(System.getProperty("user.dir"));
+							//							System.out.println(dir.getAbsolutePath());
+							//							String pwd = System.getProperty("user.dir");
+							//							return pwd;
+						}
+						else{
+							return path + " : No such a directory found!";
+						}
 					}
 				}
+				else{
+					return "No Directory Specified!";
+				}
 			}
-			else{
-				return "No Directory Specified!";
+			catch (NullPointerException e){
+				//				return "No Parent Directory";
+				return e.getMessage();
 			}
+			catch (Exception e) {
 
+				return "Error!";
+			}
 		}
-		catch (NullPointerException e){
-			return "No Parent Directory";
-		}
-		catch (Exception e) {
+		String pwd = System.getProperty("user.dir");
+		return pwd;
 
-			return "Error!";
-		}
 	}
-	
-	
-	public static void SendFile(String fileName,DataInputStream in ,DataOutputStream out ) throws IOException{
- 	   File file = new File(fileName);
- 	   if (!file.exists()){
- 		   System.out.println("No file exists!");
- 		   return;
- 	   }
- 	   out.writeUTF("okey");
- 	   String response = in.readUTF();
 
- 	   if (!response.equals("ready")){
- 		   System.out.println("File already exists in Server!");
- 		   return;
- 	   }
- 	   
- 	   
- 	   try {
-			BufferedReader br = new BufferedReader(new FileReader(fileName));
-			int line;
-			do{
-				line = br.read();
-				out.writeUTF(String.valueOf(line));
+
+	public static void SendFile(String fileName,DataInputStream in ,DataOutputStream out ) throws IOException{
+		//		System.out.println("Send Start");
+		File file = new File(fileName);
+		if (!file.exists()){
+			//System.out.println("No file exists On Server!");
+			out.writeUTF("No file exists!");
+
+			return;
+		}
+		//System.out.println("after");
+		out.writeUTF("okey");
+		String response = in.readUTF();
+
+		if (!response.equals("ready")){
+			System.out.println("File already exists in Server!");
+			return;
+		}
+
+
+		try {
+			//			BufferedReader br = new BufferedReader(new FileReader(fileName));
+			//			int line;
+			//			do{
+			//				line = br.read();
+			//				out.writeUTF(String.valueOf(line));
+			//				
+			//			}while (line != -1);
+			int count;
+			byte[] buffer = new byte[1024];
+			BufferedInputStream br = new BufferedInputStream(new FileInputStream(fileName));
+			while ((count = br.read(buffer)) > 0) {
+				out.write(buffer, 0, count);
+				String check = in.readUTF();
+				while(!check.equals("next")){
+				}
 				
-			}while (line != -1);
+			}
 			br.close();
-			
-			out.writeUTF("Transfer successful!");
-			
+			//out.writeUTF("Transfer successful!");
+
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -231,9 +272,9 @@ public class FTPServer {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
- 	   
- 	   
-    }
+
+
+	}
 
 
 }
